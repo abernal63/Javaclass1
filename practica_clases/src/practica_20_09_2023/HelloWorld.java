7ac101dc558f1b621a9278e732e7611b922068fa
Javier Diaz <javierdiazrve@gmail.com>
2023-09-20 16:54:41 +0200

    primera clase

---
+++ practica_clases/src/practica_20_09_2023/HelloWorld.java
@@ -0,0 +1,17 @@
+package practica_20_09_2023;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		final String SALUDO = "Hola Clase!!!";
+		
+		int numAlumnos = 23;
+		
+		System.out.println(SALUDO);
+		
+		System.out.println("Hoy hay " + numAlumnos + " Alumnos");
+		
+	}
+
+}
