8387a68da190d0f5e5ca3ee50c6518ceed9234e1
305526 <305526@MUA02ALP1P01121.aulas.ucam.edu>
2023-09-21 14:19:40 +0200

    clase del dia 21/09

---
+++ practica_clases/src/practica_21_09_2023/EjemploString.java
@@ -0,0 +1,14 @@
+package practica_21_09_2023;
+
+public class EjemploString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String saludo = "Hola Mundo";
+		
+		String recorte = saludo.substring(0,5);
+		
+		System.out.println(recorte);
+	}
+
+}
---
+++ practica_clases/src/practica_21_09_2023/EjemploVariables.java
@@ -0,0 +1,27 @@
+package practica_21_09_2023;
+
+public class EjemploVariables {
+
+	public static void main(String[] args) {
+		
+		byte curso = 1;
+		
+		float numFloat = 12.332f;
+		
+		double numDouble = 13.465;
+		
+		char letra = '?';
+		
+		char tabulador = '\t';
+		
+		System.out.println("Carácter: " + letra);
+		
+		System.out.println(tabulador + "Carácter: " + letra);
+		
+		System.out.println("\tCarácter: " + letra);
+		
+		System.out.println("\tCarácter: ");
+		System.out.println("esto es una \"prueba\" \\" + numDouble);
+	}
+	
+}
