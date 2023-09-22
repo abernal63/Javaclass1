9a9c5052af96af42a197ebc46e3be22bb381e2e9
305526 <305526@MUA02ALP1P01121.aulas.ucam.edu>
2023-09-22 13:34:35 +0200

    algo

---
+++ practica_clases/src/practica_22_09_2023/practica1.java
@@ -0,0 +1,37 @@
+package practica_22_09_2023;
+
+public class practica1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int divisor = 2;
+		int num1 = 8;
+		int num2 = 9;
+		int num3 = 10;
+		Integer num = 9;
+		Double num5 = 9.0;
+		
+		// CompareTo devuelve un 1 si el numero es mayor al que se le compara.
+		
+		System.out.println(num.compareTo(num1)); // num que es 9 es mayor a num1 que es 8. Devuelve 1.
+		System.out.println(num.compareTo(num2)); // num es igual a num 2. Devuelve 0.
+		System.out.println(num.compareTo(num3)); // num es menor a num 3. Devuelve -1.
+		
+		// Divisiones
+		System.out.println(num2/divisor);
+		
+		System.out.println(num5/divisor);
+		
+		System.out.println(19/num5);
+		
+		// Producto
+		
+		// 15 % 2 -> devuelve el producto de la division. 
+		// Osea el numero que sobra en la division
+		// 15 / 2 es 7 y sobra 1; 14 / 2 en 7 y sobra 0;
+		System.out.println(15%2);
+		
+		System.out.println(Math.round(Math.pow(2,3)));
+	}
+
+}
---
+++ practica_clases/src/practica_22_09_2023/tarea1.java
@@ -0,0 +1,47 @@
+package practica_22_09_2023;
+/**
+ * 
+ * @author Javier Diaz
+ * 
+ * Enunciado:
+ * Crear un programa que almacene 8 valores diferentes:
+
+	8
+	9485.58172
+	true
+	Esto es un ejemplo de tipo de dato
+	-135.248053912
+	w
+	Mi número de cliente es: 12345
+	Mi nombre es: "Pepe"
+	
+	Mostrar los valores.
+ *
+ */
+
+public class tarea1 {
+	
+	public void main(String[] args) {
+		
+		int entero = 8;
+		double decimal = 9485.58172;
+		double decimalNegativo = -135.248053912;
+		boolean booleano = 1 == 1;
+		char letra = 'w';
+		String texto = "Esto es un ejemplo de tipo de dato";
+		
+		int numeroTexto = 12345;
+		String texto2 = "Mi número de cliente es: " + numeroTexto;
+		String texto3 = "Mi nombre es: \"Pepe\"";
+		
+		System.out.println(entero);
+		System.out.println(decimal);
+		System.out.println(booleano);
+		System.out.println(texto);
+		System.out.println(decimalNegativo);
+		System.out.println(letra);
+		System.out.println(texto2);
+		System.out.println(texto3);
+	}
+
+}
