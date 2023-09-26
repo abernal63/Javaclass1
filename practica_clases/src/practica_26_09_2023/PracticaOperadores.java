0b17fbed49bbc94883836086cab621a28e43b6fe
Javier Diaz <javierdiazrve@gmail.com>
2023-09-26 10:59:55 +0200

    practica del dia 26/09/2023

---
+++ practica_clases/src/practica_26_09_2023/PracticaOperadores.java
@@ -0,0 +1,26 @@
+package practica_26_09_2023;
+
+/**
+ * 
+ * @author Javier Diaz
+ *
+ */
+public class PracticaOperadores {
+	
+	public static void main(String[] args) {
+		
+		double lado = 15.0;
+ 		
+		double areaCuadrado = Math.pow(lado, 2);
+		
+		double radio = 5.25;
+		
+		double areaCirculo = Math.PI * Math.pow(radio, 2);
+		
+		System.out.println("Area del Cuadrado: " + areaCuadrado);
+		
+		System.out.println("Area del Circulo: " + areaCirculo);
+		
+	}
+
+}
